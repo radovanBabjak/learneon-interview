c6df9501688b2b387bf7ea61fa5df8da70f27d7a
radovanBabjak <radovan@saleschamp.nl>
2022-01-24 10:24:02 +0100

    feat: fetch SpaceX rockets

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+const LAUNCHES_QUERY = `
+{
+  launchesPast(limit: 10) {
+    mission_name
+    launch_date_local
+    rocket {
+      rocket_name
+    }
+  }
+}
+`
 
 function App() {
+  const [launches, setLaunches] = useState([]);
+
+  useEffect(() => {
+    fetch('https://api.spacex.land/graphql/', {
+      method: "POST",
+      headers: { "Content-Type": "application/json"},
+      body: JSON.stringify({ query: LAUNCHES_QUERY })
+    }).then(response => response.json())
+    .then(data => setLaunches(data))
+  }, [])
+
   return (
     <div>
-      Hello World!
+      {JSON.stringify(launches, null, 2)}
     </div>
   );
 }
