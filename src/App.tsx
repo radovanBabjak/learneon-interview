ecdbf771baf7bdd5dfed9938d385da00356db1fe
radovanBabjak <radovan@saleschamp.nl>
2022-01-24 22:26:42 +0100

    feat: create input using formik and yup

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,41 @@
-import React, { useState, useEffect } from 'react';
-
-const LAUNCHES_QUERY = `
-{
-  launchesPast(limit: 10) {
-    mission_name
-    launch_date_local
-    rocket {
-      rocket_name
-    }
-  }
-}
-`
+import { Formik, Form, Field, ErrorMessage } from 'formik'; 
+import * as Yup from 'yup';
 
 function App() {
-  const [launches, setLaunches] = useState([]);
-
-  useEffect(() => {
-    fetch('https://api.spacex.land/graphql/', {
-      method: "POST",
-      headers: { "Content-Type": "application/json"},
-      body: JSON.stringify({ query: LAUNCHES_QUERY })
-    }).then(response => response.json())
-    .then(data => setLaunches(data))
-  }, [])
 
   return (
     <div>
-      {JSON.stringify(launches, null, 2)}
+      <Formik
+        initialValues={{ spaceXSearch: '' }}
+
+        validationSchema={ Yup.object({
+
+          spaceXSearch: Yup.string()
+            .min(3, 'Must be at least 3 characters long')
+            .required('Required')
+        })}
+
+        onSubmit={(values, { setSubmitting }) => {
+          setTimeout(() => {
+            alert(JSON.stringify(values, null, 2));
+            setSubmitting(false);
+          }, 400);
+        }}
+      >
+
+        {({
+          handleSubmit,
+        }) => (
+
+          <Form onSubmit={handleSubmit}>
+            <label htmlFor="spaceXSearch"> Search: </label>
+            <Field type="text" id="spaceXSearch" name="spaceXSearch" />
+            <ErrorMessage name="spaceXSearch" component="div" />
+          
+            <button type="submit">Submit</button>
+          </Form>
+        )}
+     </Formik>
     </div>
   );
 }
